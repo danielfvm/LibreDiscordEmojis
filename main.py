90371da7a59ff67fa5d16b981b20f8d249a71bfe
DanielFvM <daniel@schloegl-software.at>
2023-02-22 12:45:52 +0100

    Fixed discord not starting a second time

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import unquote
 from threading import Thread
 
+import subprocess
 import logging
 import psutil
 import sys
@@ -22,48 +23,43 @@ class KeyboardServer(BaseHTTPRequestHandler):
     def do_GET(self):
         data = unquote(self.path)[1:].replace("size=56", "size=48").replace("&quality=lossless", "")
 
+        # write url into chatbox and send it
         for key in data:
             keyboard.type(key)
             sleep(0.001)
         keyboard.type('\n')
 
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<body></body>", "utf-8"))
 
-def start_discord():
-    global proc
-
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
     target = find_executable('discord') or find_executable('discord.exe') or sys.argv[1]
-    running = 'Discord' in (p.name() for p in psutil.process_iter())
-
-    if running is True:
-        print('Please stop discord before starting this script.')
-        return
+    print('Discord executable found at:', target)
 
     if target is None:
         print('Could not find discord program, please pass the path to the executable.')
-        return
+        exit(0)
 
-    print('Discord executable found at:', target)
-    return inject(target, './script.js').proc
+    # check if discord is already running
+    running = 'Discord' in (p.name() for p in psutil.process_iter())
 
+    if running is True:
+        subprocess.Popen(target, shell=True)
+        exit(0)
 
-if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), KeyboardServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
+    # start discord with injecting script
     try:
-        p = start_discord()
+        proc = inject(target, './script.js').proc
 
+        # Start server
+        webServer = HTTPServer((hostName, serverPort), KeyboardServer)
+        print("Server started http://%s:%s" % (hostName, serverPort))
         server_thread = Thread(target=webServer.serve_forever, args=())
         server_thread.start()
 
         # Wait until discord stops
-        while p.poll() is None:
+        while proc.poll() is None:
             sleep(1)
 
     except KeyboardInterrupt:
